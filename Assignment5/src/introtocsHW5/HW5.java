a8e36128b5a9be7a2f7eb04f29481722401775bc
daniel sasson <daniels302@gmail.com>
2025-01-27 15:42:06 +0200

    finished assignment 5

--- Assignment5/src/introtocsHW5/HW5.java
+++ Assignment5/src/introtocsHW5/HW5.java
@@ -111,50 +111,29 @@ public class HW5 {
         }
         return longestCommonPrefix;
     }
-   public static boolean findIndexSortedMat(int[][] mat, int val)
-   {
-        int low = 0, high = mat.length - 1;
+    public static boolean findIndexSortedMat(int[][] mat, int val) {
+        int rows = mat.length;
+        int cols = mat[0].length;
+        int low = 0, high = rows * cols - 1;
 
-        while (low <= high)
-        {
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            int[] currentArr = mat[mid];
-            boolean isValInCurrentArr = false;
+            int row = mid / cols;  // Calculate row index
+            int col = mid % cols;  // Calculate column index
 
-            int currArrLow = 0, currArrHigh = currentArr.length - 1;
-
-            while (currArrLow <= currArrHigh)
-            {
-                int currArrMid = currArrLow + (currArrHigh - currArrLow) /2;
-                if(currentArr[currArrMid] == val)
-                {
-                    isValInCurrentArr = true;
-                    break;
-                }
-                if(currentArr[currArrMid] < val)
-                {
-                    currArrLow = currArrMid + 1;
-                }
-                else
-                {
-                    currArrHigh = currArrHigh - 1;
-                }
-            }
-            if(isValInCurrentArr)
-            {
+            if (mat[row][col] == val) {
                 return true;
             }
-            if (currentArr[0] < val)
-            {
+            if (mat[row][col] < val) {
                 low = mid + 1;
-            }
-            else
-            {
+            } else {
                 high = mid - 1;
             }
         }
         return false;
-   }
+    }
+
+
 
    public static boolean findMultiSortedArray(int [] arr, int k) {
        for (int i = 0; i < arr.length - 1; i++) {
@@ -168,6 +147,166 @@ public class HW5 {
        }
        return false;
    }
+
+    static void merge(char arr[], int left, int mid, int right)
+    {
+        // Find sizes of two subarrays to be merged
+        int rightLength = mid - left + 1;
+        int leftLength = right - mid;
+
+        // Create temp arrays
+        char leftTemp[] = new char[rightLength];
+        char rightTemp[] = new char[leftLength];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < rightLength; ++i)
+            leftTemp[i] = arr[left + i];
+        for (int j = 0; j < leftLength; ++j)
+            rightTemp[j] = arr[mid + 1 + j];
+
+        // Merge the temp arrays
+
+        // Initial indices of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray array
+        int tempArrIndexer = left;
+        while (i < rightLength && j < leftLength) {
+            if (leftTemp[i] <= rightTemp[j]) {
+                arr[tempArrIndexer] = leftTemp[i];
+                i++;
+            }
+            else {
+                arr[tempArrIndexer] = rightTemp[j];
+                j++;
+            }
+            tempArrIndexer++;
+        }
+
+        // Copy remaining elements of L[] if any
+        while (i < rightLength) {
+            arr[tempArrIndexer] = leftTemp[i];
+            i++;
+            tempArrIndexer++;
+        }
+
+        // Copy remaining elements of R[] if any
+        while (j < leftLength) {
+            arr[tempArrIndexer] = rightTemp[j];
+            j++;
+            tempArrIndexer++;
+        }
+    }
+
+    public static void mergeSort(char[] array) {
+
+        // For current size of subarrays to
+        // be merged curr_size varies from
+        // 1 to n/2
+        int curr_size;
+
+        int n = array.length;
+
+        // For picking starting index of
+        // left subarray to be merged
+        int left_start;
+
+
+        // Merge subarrays in bottom up
+        // manner. First merge subarrays
+        // of size 1 to create sorted
+        // subarrays of size 2, then merge
+        // subarrays of size 2 to create
+        // sorted subarrays of size 4, and
+        // so on.
+        for (curr_size = 1; curr_size <= n-1;
+             curr_size = 2*curr_size)
+        {
+
+            // Pick starting point of different
+            // subarrays of current size
+            for (left_start = 0; left_start < n-1;
+                 left_start += 2*curr_size)
+            {
+                // Find ending point of left
+                // subarray. mid+1 is starting
+                // point of right
+                int mid = Math.min(left_start + curr_size - 1, n-1);
+
+                int right_end = Math.min(left_start
+                        + 2*curr_size - 1, n-1);
+
+                // Merge Subarrays arr[left_start...mid]
+                // & arr[mid+1...right_end]
+                merge(array, left_start, mid, right_end);
+            }
+        }
+    }
+
+    public static int orangesRotting(int[][] grid)
+    {
+
+
+        //square matrix- num of rows=nums of cols
+        int matDimension = grid.length;
+
+        boolean allTwoFlag = true;
+        int zeroAroundCounter = 0;
+
+
+
+
+
+
+
+        boolean isChanged = false;
+        int counter = 0;
+        int[][] directionMatrix = new int[][]{
+                {0,1}, {0, -1}, {1,0}, {-1,0}
+        };
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                if(grid[i][j] != 2)
+                {
+                    continue;
+                }
+                isChanged = false;
+
+                for (int k = 0; k < directionMatrix.length; k++) {
+                    int iWithOffset = i + directionMatrix[k][0], jWithOffset = j + directionMatrix[k][1];
+                    if(iWithOffset< matDimension && iWithOffset >=0 &&
+                            jWithOffset < matDimension && jWithOffset >= 0)
+                    {
+                        if (grid[iWithOffset][jWithOffset] == 1)
+                        {
+                            grid[iWithOffset][jWithOffset] = 2;
+                            isChanged = true;
+                        }
+                    }
+                }
+               if(isChanged)
+               {
+                   counter++;
+               }
+            }
+        }
+        for (int i = 0; i < matDimension; i++) {
+            for (int j = 0; j < matDimension; j++) {
+                if(grid[i][j] == 1)
+                {
+                    return -1;
+                }
+            }
+        }
+        return counter;
+    }
+
+
+
+
+
+
+
 
 
 
@@ -190,6 +329,21 @@ public class HW5 {
                 {7,8,8}
 
         };
-        System.out.println(findIndexSortedMat(mat,6));
+        System.out.println(findIndexSortedMat(mat,8));
+
+        char[] chars = {'e','d','c','b','a'};
+        mergeSort(chars);
+        System.out.println("bla");
+
+        int[][] oranges = new int[][]
+                {
+                        {2,1,1},
+                        {0,1,1},
+                        {1,0,1}
+
+                };
+
+        int resault = orangesRotting(oranges);
+        System.out.println("bla");
     }
 }
